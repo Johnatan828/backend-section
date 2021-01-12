119f858590c9ea3d0b21eb58748ec506126a7e88
johnatan_garay <jgaray@lean-tech.io>
2021-01-12 09:05:21 -0500

    Home Route, Service & Controller created

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+require("express-async-errors");
+const Ruta = require('../routes/home.routes')
+
+module.exports = function({HomeRoutes}){
+    const router = express.Router();
+    const apiRoutes = express.Router();
+
+    apiRoutes
+    .use(express.json())
+    .use(cors())
+    .use(helmet())
+    .use(compression());
+
+    apiRoutes.use("/home", HomeRoutes);
+
+    router.use('/v1/api', apiRoutes);
+
+    return router;
+};
+
+
