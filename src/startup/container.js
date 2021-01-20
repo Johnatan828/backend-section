66980cee0cfa66a5e43a9842688332c9733843d8
johnatan_garay <jgaray@lean-tech.io>
2021-01-20 17:21:47 -0500

    [ADD Repositories added

--- src/startup/container.js
+++ src/startup/container.js
@@ -14,33 +14,45 @@ const { HomeController } = require("../controllers");
 
 //Routes
 
-const { HomeRoutes} = require("../routes/index.routes");
+const { HomeRoutes } = require("../routes/index.routes");
 const Routes = require("../routes")
 
 //Models
 
 const { User, Idea, Comment } = require('../models');
 
+//Repositories
+
+const {
+    UserRepository,
+    CommentRepository,
+    IdeaRepository
+} = require("../repositories")
+
 
 const container = createContainer();
 
 container
-.register({
-    app: asClass(app).singleton(),
-    router: asFunction(Routes).singleton(),
-    config: asValue(config)
-})
-.register({
-    HomeService: asClass(HomeService).singleton()
-}).register({
-    HomeController: asClass(HomeController.bind(HomeController)).singleton()
-}).register({
-    HomeRoutes: asFunction(HomeRoutes).singleton()
-}).register({
-    User: asValue(User),
-    Idea: asValue(Idea),
-    Comment: asValue(Comment)
-});
+    .register({
+        app: asClass(app).singleton(),
+        router: asFunction(Routes).singleton(),
+        config: asValue(config)
+    })
+    .register({
+        HomeService: asClass(HomeService).singleton()
+    }).register({
+        HomeController: asClass(HomeController.bind(HomeController)).singleton()
+    }).register({
+        HomeRoutes: asFunction(HomeRoutes).singleton()
+    }).register({
+        User: asValue(User),
+        Idea: asValue(Idea),
+        Comment: asValue(Comment)
+    }).register({
+        UserRepository: asClass(UserRepository).singleton(),
+        IdeaRepository: asClass(IdeaRepository).singleton(),
+        CommentRepository: asClass(CommentRepository).singleton()
+    })
 
 
 module.exports = container;
